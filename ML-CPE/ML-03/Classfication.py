086d30b09cf319f0cff142cf5a88e82607a59b69
DAMMBRO-COMEON <Weerapat.wd@gmail.com>
2026-08-05 23:46:41 +0700

    Delete ML-CPE/ML-03 directory

--- ML-CPE/ML-03/Classfication.py
+++
@@ -1,6 +0,0 @@
-import numpy as np 
-import pandas as pd 
-houseprice = pd.read_csv('C:\Users\SW02\Desktop\Wine-Quality-Dataset\ML-03\Lab03\lab03\submission_house_price.csv')
-sub  = pd.read_csv('C:\Users\SW02\Desktop\Wine-Quality-Dataset\ML-03\Lab03\lab03\submission.csv')
-from pycaret.classification import *
-houseprice.head()
